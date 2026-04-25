aede533cb59ebbc87310f075f1ceb9b6e31ea80b
Aanastasia18 <aanastasia2000@gmail.com>
2026-04-25 15:31:56 +0300

    Adds /healt route to the routers

--- app/routers/router.py
+++ app/routers/router.py
@@ -5,3 +5,8 @@ router = APIRouter()
 @router.get("/")
 async def root():
     return {"message": "Hello world!"}
+
+
+@router.get("/health")
+async def health():
+    return {"message": "You are healthy!"}
